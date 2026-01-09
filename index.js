b814fd84e64e52e2eb006e25956ceb72b160c8dd
MdMahdiHasanTazelly <mahdihasantazelly@gmail.com>
2026-01-09 17:24:52 +0600

    Updated frintend origin in cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://ecafe-backend.netlify.app"
+        "https://ecafefrontend.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
